8eb26eec1cfc39600bc750c293a0d8e72de2cea7
Wuuu <mrwu0911@163.com>
2024-02-01 08:53:22 +0800

    init etcd

--- common/discovery/register.go
+++ common/discovery/register.go
@@ -4,6 +4,7 @@ import (
 	"common/config"
 	"common/logs"
 	"context"
+	"encoding/json"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"time"
 )
@@ -17,7 +18,7 @@ type Register struct {
 	leaseId     clientv3.LeaseID                       // 租约id
 	DialTimeout int                                    // 超时时间
 	ttl         int                                    // 租约时间
-	keepAliveCh <-chan clientv3.LeaseKeepAliveResponse // 租约心跳
+	keepAliveCh <-chan *clientv3.LeaseKeepAliveResponse // 租约心跳
 	info        Server                                 // 注册的Server信息
 	closeCh     chan struct{}                          // close标识
 }
@@ -49,7 +50,16 @@ func (r *Register) Register(conf config.EtcdConf) error {
 	}
 
 	r.info = info
-	return r.register()
+	if err = r.register(); err != nil {
+		return err
+	}
+	// 给etcd注销的通道赋一个初始容量
+	r.closeCh = make(chan struct{})
+
+	// 放入协程中，根据心跳结果，做响应操作
+	go r.watcher()
+
+	return nil
 
 }
 
@@ -58,22 +68,58 @@ func (r *Register) register() error {
 	// 1. 创建租约
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(r.DialTimeout))
 	defer cancel()
-	err := r.createLease(ctx, r.info.Ttl)
+	var err error
+	err = r.createLease(ctx, r.info.Ttl)
 	if err != nil {
-		// todo: 创建租约失败的处理
 		return err
 	}
 
 	// 2. 心跳检测
+	if r.keepAliveCh, err = r.keepAlive(ctx); err != nil {
+		return err
+	}
 	// 3. 绑定租约
-	return nil
+	data, err := json.Marshal(r.info)
+	if err != nil {
+		return err
+	}
+	return r.bindLease(ctx, r.info.BuildRegisterKey(), string(data))
 }
 
+// 创建租约
 func (r *Register) createLease(ctx context.Context, ttl int64) error {
 	grant, err := r.etcdCli.Grant(ctx, ttl)
 	if err != nil {
 		logs.Error("==> create lease failed err: %v", err)
+		return err
 	}
 	r.leaseId = grant.ID
 	return nil
 }
+
+// bindLease 绑定租约
+func (r *Register) bindLease(ctx context.Context, key, value string) error {
+	// 绑定租约本质上就是针对与etcd的一个put操作
+	_, err := r.etcdCli.Put(ctx, key, value, clientv3.WithLease(r.leaseId))
+	if err != nil {
+		logs.Error("==> bind lease failed err: %v", err)
+		return err
+	}
+	return nil
+}
+
+// keepAlive 心跳检测
+func (r *Register) keepAlive(ctx context.Context) (<-chan *clientv3.LeaseKeepAliveResponse, error) {
+	keepAliveResponses, err := r.etcdCli.KeepAlive(ctx, r.leaseId)
+	if err != nil {
+		logs.Error("==> keep alive failed err: %v", err)
+		return keepAliveResponses, err
+	}
+	return keepAliveResponses, nil
+}
+
+// watcher etcd连接的监听，包括：续约 新注册 注销
+func (r *Register) watcher() {
+	
+}
+
--- common/discovery/server.go
+++ common/discovery/server.go
@@ -1,5 +1,7 @@
 package discovery
 
+import "fmt"
+
 // 向etcd中注册的Server信息
 type Server struct {
 	Name    string `json:"name"`
@@ -7,4 +9,11 @@ type Server struct {
 	Weight  int    `json:"weight"`
 	Version string `json:"version"`
 	Ttl     int64  `json:"ttl"`
+}
+
+func (s Server) BuildRegisterKey() string {
+	if len(s.Version) == 0 {
+		return fmt.Sprintf("/%s/%s", s.Name, s.Addr)
+	}
+	return fmt.Sprintf("/%s/%s/%s", s.Name,s.Version, s.Addr)
 }
